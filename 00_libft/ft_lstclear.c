0bf40a1b0cbab25271a60c8c338d8056e43fe14b
grokcamp <116454358+grokcamp@users.noreply.github.com>
2023-12-21 22:54:14 +0100

    Add files via upload
    added bonus exercises: ft_lstnew, ft_lstadd_front, ft_lstsize, ft_lstlast, ft_lstadd_back, ft_lstdelone, ft_lstclear
    both ft_lstdelone & ft_lstclear need reexamination, as I haven't fully wrapped my head around how they work yet -- my test mains aren't working, and I'm getting an error on free() when testing, but according to other students' material, the core functions appear correct.
    last two exercises in BONUS to complete are ft_lstiteri and ft_lstmap, then write the makefile, then we're done!

--- 00_libft/ft_lstclear.c
+++ 00_libft/ft_lstclear.c
@@ -1,44 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_striteri.c                                      :+:      :+:    :+:   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gcampbel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/18 22:46:10 by gcampbel          #+#    #+#             */
-/*   Updated: 2023/12/21 19:28:14 by gcampbel         ###   ########.fr       */
+/*   Created: 2023/12/21 22:25:41 by gcampbel          #+#    #+#             */
+/*   Updated: 2023/12/21 22:39:48 by gcampbel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
-	unsigned int	i;
+	t_list	*ticker;
 
-	i = 0;
-	while (s[i])
+	if (!lst || !del || !(*lst))
+		return ;
+	while (*lst)
 	{
-		(*f)(i, &s[i]);
-		i++;
+		ticker = (*lst)->next;
+		ft_lstdelone(*lst, del);
+		*lst = ticker;
 	}
+	free(*lst);
+	*lst = NULL;
 }
-/*
-static void	ft_lower(unsigned int i, char *c)
-{
-	while (c[i])
-	{
-		if ('A' <= c[i] && c[i] <= 'Z')
-			c[i] += 32;
-		i++;
-	}
-}
-
-int	main(void)
-{
-	char	str[] = "HELLO WORLD";
-
-	printf("Original string was: %s\n", str);
-	ft_striteri(str, ft_lower);
-	printf("Output string is: %s\n", str);
-}*/
--- 00_libft/libft.h
+++ 00_libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: gcampbel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 18:14:50 by gcampbel          #+#    #+#             */
-/*   Updated: 2023/12/19 21:42:03 by gcampbel         ###   ########.fr       */
+/*   Updated: 2023/12/21 22:48:42 by gcampbel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef LIBFT_H
@@ -61,5 +61,13 @@ typedef struct		s_list
 	void		*content;
 	struct s_list	*next;
 }			t_list;
+
+t_list	*ft_lstnew(void *content);
+void    ft_lstadd_front(t_list **lst, t_list *new);
+int     ft_lstsize(t_list *lst);
+t_list  *ft_lstlast(t_list *lst);
+void    ft_lstadd_back(t_list **lst, t_list *new);
+void    ft_lstdelone(t_list *lst, void (*del)(void *));
+void    ft_lstclear(t_list **lst, void (*del)(void *));
 
 #endif
